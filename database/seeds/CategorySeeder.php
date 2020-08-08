432e7e12e4312e76db375cd5700b69e373598988
furkanaltaca <furkanaltaca@gmail.com>
2020-08-08 19:46:48 +0300

    CategorySeeder added. Category table migration added.

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            'C#',
+            '.Net Core',
+            'JavaScript',
+            'ASP.NET',
+            'Laravel',
+            'HTML',
+            'CSS',
+            'jQuery'
+        ];
+
+        foreach ($categories as $category) {
+            DB::table('categories')->insert([
+                'name' => $category,
+                'slug' => Str::slug($category, '-'),
+                'created_at'=>now(),
+                'updated_at'=>now()
+            ]);
+        }
+    }
+}
