aa4ed04fb07839262cc3a1e0f70d4f95fdf61db3
furkanaltaca <furkanaltaca@gmail.com>
2020-08-17 13:58:57 +0300

    Login page added.

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/admin/panel','Back\DashboardController@index')->name('admin.dashboard');
+Route::get('/admin/giris','Back\AuthController@login')->name('admin.login');
 
 
 
