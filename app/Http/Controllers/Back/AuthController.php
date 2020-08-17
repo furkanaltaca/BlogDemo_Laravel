53ed5bcf8f467c9a31c3350846460513c493226c
furkanaltaca <furkanaltaca@gmail.com>
2020-08-17 16:01:18 +0300

    Some edits was done about authentication. AuthController added and codes added for login and logout.

--- app/Http/Controllers/Back/AuthController.php
+++ app/Http/Controllers/Back/AuthController.php
@@ -4,11 +4,33 @@ namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login()
     {
         return view('back.auth.login');
+    }
+
+    public function loginPost(Request $request)
+    {
+        $isLogin = Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        if (!$isLogin) {
+            return redirect()->route('admin.login')->withErrors('Email veya şifre hatalı!');
+        }
+        
+        return redirect()->route('admin.dashboard');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('admin.login');
     }
 }
