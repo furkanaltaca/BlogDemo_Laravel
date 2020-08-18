a2fac0d390891e48ff045ebc7ec469d806312804
furkanaltaca <furkanaltaca@gmail.com>
2020-08-18 21:44:58 +0300

    Toastr library added to project.

--- resources/views/back/layouts/master.blade.php
+++ resources/views/back/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="tr">
 
 <head>
 
@@ -16,6 +16,7 @@
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
     <link href="{{ asset('back/') }}/css/sb-admin-2.min.css" rel="stylesheet">
+    @toastr_css
 
     @stack('css')
 </head>
@@ -64,6 +65,8 @@
     <script src="{{ asset('back/') }}/vendor/chart.js/Chart.min.js"></script>
     <script src="{{ asset('back/') }}/js/demo/chart-area-demo.js"></script>
     <script src="{{ asset('back/') }}/js/demo/chart-pie-demo.js"></script>
+    @toastr_js
+    @toastr_render
 
     @stack('js')
 </body>
