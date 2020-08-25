070bc4bce000e6f25709a0f366bf2f54d149d0a9
furkanaltaca <furkanaltaca@gmail.com>
2020-08-25 18:04:16 +0300

    Change function name in category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,7 +9,7 @@ class Category extends Model
     protected $table='categories';
     protected $fillable=['name','slug'];
 
-    public function getArticlesCount(){
-        return $this->hasMany('App\Models\Article','category_id','id')->where('status',1)->count();
+    public function articles(){
+        return $this->hasMany('App\Models\Article','category_id','id')->where('status',1)->get();
     }
 }
