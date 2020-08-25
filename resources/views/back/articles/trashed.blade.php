421fee9990d78c6c66224522bcfb9558e7c87332
furkanaltaca <furkanaltaca@gmail.com>
2020-08-25 22:13:58 +0300

    Edit trashed.blade.php according ajax

--- resources/views/back/articles/trashed.blade.php
+++ resources/views/back/articles/trashed.blade.php
@@ -1,22 +1,24 @@
 @extends('back.layouts.master')
-@section('title','Admin Tüm Makaleler - Blog Demo')
-@section('pageHeading','Tüm Makaleler')
+@section('title','Silinen Makaleler - Blog Demo Admin')
+@section('pageHeading','Silinen Makaleler')
 
+@push('url')
+<script type="text/javascript">
+    var urlGetAllTrashedArticle ='{{ route("admin.makaleler.trashed") }}';
+    var urlRecoverArticle='{{ route("admin.makaleler.recover") }}';
+    var urlHardDeleteArticle='{{ route("admin.makaleler.hardDelete") }}';
+</script>
+@endpush
 
 @section('content')
 
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">
-            Makaleler
-            <span class="float-right">
-                <strong>{{ $articles->count() }}</strong> makale bulundu.
-            </span>
-        </h6>
+        <h6 class="m-0 font-weight-bold text-primary">@yield('pageHeading')</h6>
     </div>
     <div class="card-body">
         <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <table class="table table-bordered" id="trashedArticleDatatable" width="100%" cellspacing="0">
                 <thead>
                     <tr>
                         <th>Fotoğraf</th>
@@ -27,43 +29,6 @@
                         <th>İşlemler</th>
                     </tr>
                 </thead>
-                <tbody>
-                    @foreach ($articles as $article)
-                    <tr>
-                        <td>
-                            <a href="{{ asset($article->image) }}" target="_blank" data-toggle="popover-hover"
-                                data-img="{{ asset($article->image) }}">
-                                Fotoğraf
-                                {{-- {{ Str::limit(Str::substr($article->image,22),16) }} --}}
-                            </a>
-                        </td>
-                        <td class="align-middle">{{ $article->title }}</td>
-                        <td class="align-middle">{{ $article->getCategory->name }}</td>
-                        <td class="align-middle text-center">{{ $article->hit }}</td>
-                        <td class="align-middle">{{ $article->created_at->diffforhumans() }}</td>
-                        <td class="align-middle text-center">
-                            <div>
-                                <button type="button" class="btn btn-sm btn-primary btn-circle dropdown-toggle"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-                                <div class="dropdown-menu">
-                                    <a href="{{ route('admin.makaleler.recover',$article->id) }}"
-                                        article-id="{{ $article->id }}"
-                                        class="dropdown-item">
-                                        <i class="fas fa-trash-restore"></i>
-                                        Kurtar
-                                    </a>
-                                    <a href="{{ route('admin.makaleler.hardDelete',$article->id) }}"
-                                        article-id="{{ $article->id }}"
-                                        class="dropdown-item">
-                                        <i class="fa fa-times"></i>
-                                        Kalıcı Olarak Sil
-                                    </a>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
             </table>
         </div>
     </div>
@@ -73,13 +38,8 @@
 
 
 @push('css')
-<link href="{{ asset('back/') }}/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 @endpush
 
 @push('js')
-<script src="{{ asset('back/') }}/vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="{{ asset('back/') }}/vendor/datatables/dataTables.bootstrap4.min.js"></script>
-<script src="{{ asset('back/') }}/js/demo/datatables-demo.js"></script>
-
 <script src="{{ asset('js/back/articles/trashed.js') }}" type="text/javascript"></script>
 @endpush
