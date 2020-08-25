ec4c094b92002081c269ca7c78b2f4f5fc31e327
furkanaltaca <furkanaltaca@gmail.com>
2020-08-25 22:10:24 +0300

    Add codes in to article/trashed.js

--- public/js/back/articles/trashed.js
+++ public/js/back/articles/trashed.js
@@ -1,10 +1,85 @@
 $(document).ready(function () {
-    // $('[data-toggle="popover-hover"]').popover({
-    //     html: true,
-    //     trigger: "hover",
-    //     placement: "bottom",
-    //     content: function () {
-    //         return '<img src="' + $(this).data("img") + '" / class="w-50">';
-    //     },
-    // });
+    //ARRANGE
+    var trashedArticleDatatable = $("#trashedArticleDatatable").DataTable({
+        // dom: "Bfrtip",
+        // processing: true,
+        // serverSide: true,
+        // responsive: true,
+        pageLength: 5,
+        lengthMenu: [5, 10, 25, 50, 100],
+        ajax: {
+            type: "GET",
+            url: urlGetAllTrashedArticle,
+            data: {},
+            dataType: "json",
+        },
+        columns: [
+            { data: "image" },
+            { data: "title" },
+            { data: "category.name" },
+            { data: "hit" },
+            { data: "created_at" },
+            { data: "actions" },
+        ],
+        drawCallback: function () {
+            $('[data-toggle="showImage-popover"]').popover({
+                html: true,
+                trigger: "hover",
+                placement: "bottom",
+                content: function () {
+                    return '<img src="' + $(this).data("img") + '" / class="w-50">';
+                },
+            });
+        }
+    });
+    var btnRecoverArticle = '[data-event="RecoverArticle"]';
+    var btnHardDeleteArticle = '[data-event="HardDeleteArticle"]';
+    //END ARRANGE
+    //-------------------------------------------------------------------------
+    //EVENT
+    trashedArticleDatatable.on('click', btnRecoverArticle, function () {
+        var params = {
+            id: $(this).data("article-id")
+        };
+
+        $.ajax({
+            type: "PATCH",
+            url: urlRecoverArticle,
+            data: params,
+            dataType: 'json'
+        }).done(function (result) {
+            toastr.success(result.message, result.messageTitle);
+        }).fail(function (result) {
+            toastr.error(result.message, "Hata");
+        }).always(function(){
+            reloadDatatable();
+        });
+    });
+
+    trashedArticleDatatable.on("click", btnHardDeleteArticle, function () {
+        var params = {
+            id: $(this).data('article-id')
+        };
+
+        $.ajax({
+            type: "DELETE",
+            url: urlHardDeleteArticle,
+            data: params,
+            dataType: "json",
+        }).done(function (result) {
+            toastr.success(result.message, result.messageTitle);
+        }).fail(function (result) {
+            toastr.error(result.responseJSON.message, "Hata");
+        }).always(function () {
+            reloadDatatable();
+        });
+    });
+    //END EVENT
+    //-------------------------------------------------------------------------
+    //FUNCTIONS
+    function reloadDatatable() {
+        trashedArticleDatatable.ajax.reload();
+    }
+    //END FUNCTIONS
+    //-------------------------------------------------------------------------
 });
