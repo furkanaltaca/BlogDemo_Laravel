acc6e910b7b1022310fde075e2d467a827830a9d
furkanaltaca <furkanaltaca@gmail.com>
2020-08-25 22:13:32 +0300

    Edit codes in articles/index.blade.php

--- resources/views/back/articles/index.blade.php
+++ resources/views/back/articles/index.blade.php
@@ -5,7 +5,7 @@
 @push('url')
 <script type="text/javascript">
     var urlGetAllArticle ='{{ route("admin.makaleler.index") }}';
-    var urlDeleteArticle = '{{ route("admin.makaleler.delete") }}';
+    var urlDeleteArticle = '{{ route("admin.makaleler.destroy",'') }}';
     var urlUpdateArticleStatus='{{ route("admin.makaleler.updateStatus") }}';
 </script>
 @endpush
@@ -13,12 +13,7 @@
 @section('content')
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">
-            Makaleler
-            <span class="float-right">
-                <strong>{{ $articles->count() }}</strong> makale bulundu.
-            </span>
-        </h6>
+        <h6 class="m-0 font-weight-bold text-primary">@yield('pageHeading')</h6>
     </div>
     <div class="card-body">
         <div class="table-responsive">
@@ -43,11 +38,8 @@
 
 
 @push('css')
-<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
 @endpush
 
 @push('js')
-<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
-
 <script src="{{ asset('js/back/articles/index.js') }}" type="text/javascript"></script>
 @endpush
